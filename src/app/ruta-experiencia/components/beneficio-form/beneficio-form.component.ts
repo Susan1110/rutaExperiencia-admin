43550a10d208508f181be9893a6526b13d330dd6
FernandoBalbin <72724516@continental.edu.pe>
2023-05-31 16:19:33 -0500

    Componente Beneficio Actualizado

--- src/app/ruta-experiencia/components/beneficio-form/beneficio-form.component.ts
+++ src/app/ruta-experiencia/components/beneficio-form/beneficio-form.component.ts
@@ -1,90 +1,93 @@
-import { Component,Input,Output,EventEmitter,ViewChild,ElementRef } from '@angular/core';
-import { Beneficio, NuevoBeneficio } from '../../Interfaces/ruta-beneficio.interface';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import {
+  Beneficio,
+  NuevoBeneficio,
+} from '../../Interfaces/ruta-beneficio.interface';
 import { ModalService } from '../../services/modal.service';
 import { BeneficioService } from '../../services/beneficio.service';
 import { AuthService } from './../../../auth/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-beneficio-form',
   templateUrl: './beneficio-form.component.html',
-  styleUrls: ['./beneficio-form.component.css']
+  styleUrls: ['./beneficio-form.component.css'],
 })
 export class BeneficioFormComponent {
   beneficioForm: FormGroup;
 
   // @ViewChild('txtDescripcionBeneficio') txtDescripcionBeneficio!: ElementRef<HTMLInputElement>
-  get beneficio(){
-    return this.beneficioService.beneficio
+  get beneficio() {
+    return this.beneficioService.beneficio;
   }
   get funcion() {
-    return this.modalService.funcionFormularioBeneficio
+    return this.modalService.funcionFormularioBeneficio;
   }
   constructor(
     private modalService: ModalService,
     private authService: AuthService,
-    private  beneficioService:  BeneficioService,
+    private beneficioService: BeneficioService,
     private toastr: ToastrService,
-    private formBuilder: FormBuilder,
-   
-  ) { 
-    this.beneficioForm= this.formBuilder.group({
+    private formBuilder: FormBuilder
+  ) {
+    this.beneficioForm = this.formBuilder.group({
       descripcion: ['', Validators.required],
-    })
-    if(this.funcion=='agregar'){
-      this.beneficioForm= this.formBuilder.group({
+    });
+    if (this.funcion == 'agregar') {
+      this.beneficioForm = this.formBuilder.group({
         descripcion: ['', Validators.required],
-      })
+      });
     }
-   if(this.funcion=='editar'){
-    this.beneficioForm= this.formBuilder.group({
-      descripcion: [this.beneficio.BeDescripcion ||'', Validators.required],
-    })
-   }
+    if (this.funcion == 'editar') {
+      this.beneficioForm = this.formBuilder.group({
+        descripcion: [this.beneficio.BeDescripcion || '', Validators.required],
+      });
+    }
   }
-  modal: boolean = true
+  modal: boolean = true;
 
   cerrarFormulario() {
-    this.modalService.cerrarFormularioBeneficio()
+    this.modalService.cerrarFormularioBeneficio();
   }
 
-
-  agregarBeneficio(){
-    if(this.beneficioForm.valid){
-      const carrera = this.authService.usuario.idCarrera
-      const data:NuevoBeneficio = {
-      "BeDescripcion": this.beneficioForm.value.descripcion,
-      "IdCarrera": carrera!};
-      this.beneficioService.subirBeneficio(data).subscribe(response=>{
+  agregarBeneficio() {
+    if (this.beneficioForm.valid) {
+      const carrera = this.authService.usuario.idCarrera;
+      const data: NuevoBeneficio = {
+        BeDescripcion: this.beneficioForm.value.descripcion,
+        IdCarrera: carrera!,
+      };
+      this.beneficioService.subirBeneficio(data).subscribe((response) => {
         console.log(response);
-        this.toastr.success('Contenido registrado exitosamente.')
-        this.modalService.cerrarFormularioBeneficio()
-      })
+        this.toastr.success('Contenido registrado exitosamente.');
+        this.modalService.cerrarFormularioBeneficio();
+      });
       console.log(data);
     }
   }
-  editarBeneficio(){
-    if(this.beneficioForm.valid){
-      const idBeneficio= this.beneficio.IdBeneficio
-      const carrera = this.authService.usuario.idCarrera
+  editarBeneficio() {
+    if (this.beneficioForm.valid) {
+      const idBeneficio = this.beneficio.IdBeneficio;
+      const carrera = this.authService.usuario.idCarrera;
       const data: NuevoBeneficio = {
-        "BeDescripcion": this.beneficioForm.value.descripcion,
-        "IdCarrera": carrera!
-      }
-      this.beneficioService.editarBeneficio(idBeneficio, data).subscribe(
-        {
-          next: () => {
-            this.toastr.success('Experiencia Editada')
-          },
-          error: () => this.toastr.error('No se pudo editar el beneficio')
-        }
-      )
+        BeDescripcion: this.beneficioForm.value.descripcion,
+        IdCarrera: carrera!,
+      };
+      this.beneficioService.editarBeneficio(idBeneficio, data).subscribe({
+        next: () => {
+          this.toastr.success('Experiencia Editada'),
+          this.modalService.cerrarFormularioBeneficio();
+        },
+        error: () => this.toastr.error('No se pudo editar el beneficio'),
+      });
     }
   }
-
-  
 }
-
--- src/app/ruta-experiencia/components/beneficio-list/beneficio-list.component.ts
+++ src/app/ruta-experiencia/components/beneficio-list/beneficio-list.component.ts
@@ -4,77 +4,79 @@ import { AuthService } from '../../../auth/services/auth.service';
 import { ModalService } from '../../services/modal.service';
 import { ToastrService } from 'ngx-toastr';
 import { Beneficio } from '../../Interfaces/ruta-beneficio.interface';
-
 @Component({
   selector: 'app-beneficio-list',
   templateUrl: './beneficio-list.component.html',
-  styleUrls: ['./beneficio-list.component.css']
+  styleUrls: ['./beneficio-list.component.css'],
 })
 export class BeneficioListComponent {
+  filas: number = this.beneficios.length + 1;
+  ciclos: number = 10;
 
-  filas: number = this.beneficios.length + 1
-  ciclos: number = 10
-  
-  get beneficios(): Beneficio[]{
-    console.log(this.beneficioService.beneficios)
-    return this.beneficioService.beneficios
+  get beneficios(): Beneficio[] {
+    console.log(this.beneficioService.beneficios);
+    return this.beneficioService.beneficios;
   }
-  
+
   ngOnInit(): void {
-    this.beneficioService.buscarBeneficio().subscribe()
+    this.beneficioService.buscarBeneficio().subscribe();
   }
   constructor(
-    private beneficioService: BeneficioService, 
+    private beneficioService: BeneficioService,
     private authService: AuthService,
     private modalService: ModalService,
-    ) { }
+    private toastr: ToastrService
+  ) {}
 
   get usuario() {
-    return this.authService.usuario
+    return this.authService.usuario;
   }
- eliminar(beneficio: Beneficio){
-  this.beneficioService.eliminarBeneficio(beneficio.IdBeneficio).subscribe()
- }
-  
-  abrirModalEditar(funcion:'editar',beneficio: Beneficio){
-    this.modalService.abrirFormularioBeneficio(funcion)
-    this.beneficioService.obtenerBeneficio(beneficio)
+  eliminar(beneficio: Beneficio) {
+    this.beneficioService
+      .eliminarBeneficio(beneficio.IdBeneficio)
+      .subscribe((response) => {
+        console.log(response);
+        this.toastr.success('Beneficio eliminado exitosamente.');
+      });
   }
-  
+
+  abrirModalEditar(funcion: 'editar', beneficio: Beneficio) {
+    this.modalService.abrirFormularioBeneficio(funcion);
+    this.beneficioService.obtenerBeneficio(beneficio);
+  }
+
   abrirModal(funcion: 'agregar' | 'editar') {
-    this.modalService.abrirFormularioBeneficio(funcion)
+    this.modalService.abrirFormularioBeneficio(funcion);
   }
   gridLayout() {
     return {
-      'display': 'grid',
+      display: 'grid',
       'grid-template-columns': `repeat(${this.ciclos},1fr)`,
       'grid-template-rows': `repeat(${this.filas},1fr)`,
-      'gap': '16px 30px'
-    }
+      gap: '16px 30px',
+    };
   }
 
   posicionBeneficio(cicloInicio: number = 1, cicloFin: number = 10) {
-    const largo = cicloFin - cicloInicio + 1
-    const color = this.background(cicloInicio, cicloFin, largo)
+    const largo = cicloFin - cicloInicio + 1;
+    const color = this.background(cicloInicio, cicloFin, largo);
     return {
       'grid-column': `${cicloInicio} / span ${largo}`,
-      'background': color,
-    }
+      background: color,
+    };
   }
 
   background(inicio: number, fin: number, expansion: number) {
-    let color = ''
+    let color = '';
     if (expansion > 1) {
-      let gradient = ''
+      let gradient = '';
       for (let index = inicio; index <= fin; index++) {
-        gradient += `, var(--carrera-color-${index})`
+        gradient += `, var(--carrera-color-${index})`;
       }
-      color = `linear-gradient(90deg ${gradient})`
+      color = `linear-gradient(90deg ${gradient})`;
+    } else {
+      color = `var(--carrera-color-${inicio})`;
     }
-
-    else {
-      color = `var(--carrera-color-${inicio})`
-    }
-    return color
+    return color;
   }
 }
