ba1377f13392078c9415445a473b5dc55bfb4929
byauricasa <d4160@outlook.com>
2023-06-04 23:08:30 -0500

    Fix express deploy

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const app = express();
 const PORT = 80; 
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/ruta-experiencia'));
-app.use(express.static(__dirname + '/cliente-web'));
+app.use('/admin', express.static(__dirname + '/dist/ruta-experiencia'));
+app.use('/', express.static(__dirname + '/cliente-web'));
 
 // Send all requests to index.html
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
-});
 app.get('/admin', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ruta-experiencia/index.html'));
 });
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
 });
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,24 +4,34 @@ import { LoginComponent } from './auth/login/login.component';
 // import { MainPageComponent } from './main/main-page/main-page.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'main',
-    // component: MainPageComponent
-    loadChildren: () => import('./main/main.module').then(m => m.MainModule),
-  },
+  { path: '', children: [
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: 'main',
+      // component: MainPageComponent
+      loadChildren: () => import('./main/main.module').then(m => m.MainModule),
+    },
+    {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full',
+    },
+  ]},
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'admin',
     pathMatch: 'full',
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { 
+    // Configura la ruta base
+    useHash: false, // Usa el hash en la URL (opcional, si es necesario)
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
