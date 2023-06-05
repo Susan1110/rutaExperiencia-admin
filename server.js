882e1a9c6f21a353a7f5eaacee4468374dd22f74
byauricasa <d4160@outlook.com>
2023-06-04 19:37:10 -0500

    Change port 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 5000; 
+const PORT = 80; 
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/ruta-experiencia'));
