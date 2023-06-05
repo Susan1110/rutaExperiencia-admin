d1630be35e8b228732b2f3f6e716bf9ba0cb4e02
byauricasa <d4160@outlook.com>
2023-06-04 20:57:53 -0500

    Added web client

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const PORT = 80;
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/ruta-experiencia'));
+app.use(express.static(__dirname + '/cliente-web'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
+});
+app.get('/admin', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ruta-experiencia/index.html'));
+});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
 });
 
 // default Heroku port
