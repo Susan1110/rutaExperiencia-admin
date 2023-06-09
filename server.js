12d60a9eace97b90984cf1bad8f7d39d8ac9297a
byauricasa <d4160@outlook.com>
2023-06-09 16:46:43 -0500

    New style front

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const PORT = 80;
 
 // Serve static files
 app.use('/admin', express.static(__dirname + '/dist/ruta-experiencia'));
+app.use('/home', express.static(__dirname + '/cliente-web'));
+app.use('/home2', express.static(__dirname + '/ruta-experiencia-web'));
 app.use('/', express.static(__dirname + '/cliente-web'));
 
 // Send all requests to index.html
 app.get('/admin', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ruta-experiencia/index.html'));
+});
+app.get('/home', function(req, res) {
+  res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
+});
+app.get('/home2', function(req, res) {
+  res.sendFile(path.join(__dirname + '/ruta-experiencia-web/index.html'));
 });
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/cliente-web/index.html'));
