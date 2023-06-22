8860fb3745e283b1af495f5ac950ff47cc35cf1e
Renzo <72679166@continental.edu.pe>
2023-06-22 00:13:06 -0500

    Actualizando css

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -1,1 +1,70 @@
 export const API_URL = 'http://144.22.59.87:4040';
+
+export const iconos = [
+  {
+    nombre: 'Análisis Químico',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_analisis-quimico.png',
+  },
+  {
+    nombre: 'Cámara de Video',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_camara-de-video.png',
+  },
+  {
+    nombre: 'Certificación',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_certificacion.png',
+  },
+  {
+    nombre: 'DevApp',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_devApp.png',
+  },
+  {
+    nombre: 'Diseño App',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_diseno-app.png',
+  },
+  {
+    nombre: 'Estadística',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_estadistica.png',
+  },
+  {
+    nombre: 'Física',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_fisica.png',
+  },
+  {
+    nombre: 'Galería de Imágenes',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_galeria-de-imagenes.png',
+  },
+  {
+    nombre: 'Google Docs',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_google-docs.png',
+  },
+  {
+    nombre: 'Libro de Lectura',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_libro-de-lectura.png',
+  },
+  {
+    nombre: 'Libro',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_libro.png',
+  },
+  {
+    nombre: 'Moneda',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_moneda.png',
+  },
+  {
+    nombre: 'Portafolio',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_portafolio.png',
+  },
+  {
+    nombre: 'Simulador',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_simulador.png',
+  },
+  {
+    nombre: 'Vista 360',
+    url: 'https://objectstorage.sa-santiago-1.oraclecloud.com/p/uQX9oUnIJfHFnt3Mu9is1SPIv95TulLc0GS_DNTmRKIQrqZuydNm477vBhROHfBp/n/axth4ig2xaeu/b/rutaexperiencia-media/o/icon_vista-360.png',
+  },
+];
+
+export const tipoMedia = [
+  { id: 1, nombre: 'Imagen' },
+  { id: 2, nombre: 'Video' },
+  { id: 3, nombre: 'Imagen360' },
+];
